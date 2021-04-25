daf6c51a7aa470c48c044fdaee0dd38e21207634
Can GULDOGAN <cguldogan@gmail.com>
2021-04-25 01:52:31 +0100

    feat: projects

---
+++ MicroService-2/Controllers/ResolverController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResolverController : ControllerBase
+    {
+        public ResolverController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("MicroService-2");
+        }
+    }
+}
